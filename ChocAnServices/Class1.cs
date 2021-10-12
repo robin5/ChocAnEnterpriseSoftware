d4a97932c8116d260172bbb0da9b341836e69285
Robin Murray <r.murray@students.clark.edu>
2021-10-11 23:04:30 -0700

    Add project files.

---
+++ ChocAnServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChocAnServices
+{
+    public class Class1
+    {
+    }
+}
---
+++ MainAccountingProcedure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainAccountingProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
