6ff766334af53e42be89a505e6f4238c0f4e10f2
Robin Murray <r.murray@students.clark.edu>
2021-10-11 23:52:59 -0700

    Defined member, provider, provider service, and provider transaction entities.

---
+++ ChocAnServices/Models/ProviderServiceEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChocAnServices.Models
+{
+    public class ProviderServiceEntity
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
