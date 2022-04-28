bcdfe09b3f65214d69791a0e98eff20cde2fd665
Robin Murray <r.murray@students.clark.edu>
2022-04-27 21:59:59 -0700

    Adds sending RabbitMQ message when AccountsPayableReport is generated

---
+++ ChocAn.AccountsPayableReportServiceApi/Program.cs
@@ -0,0 +1,100 @@
+// **********************************************************************************
+// * Copyright (c) 2021 Robin Murray
+// **********************************************************************************
+// *
+// * File: Program.cs
+// *
+// * Description: Application startup file for ChocAn.AccountsPayableReportServiceApi project.
+// *
+// **********************************************************************************
+// * Author: Robin Murray
+// **********************************************************************************
+// *
+// * Granting License: The MIT License (MIT)
+// * 
+// *   Permission is hereby granted, free of charge, to any person obtaining a copy
+// *   of this software and associated documentation files (the "Software"), to deal
+// *   in the Software without restriction, including without limitation the rights
+// *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// *   copies of the Software, and to permit persons to whom the Software is
+// *   furnished to do so, subject to the following conditions:
+// *   The above copyright notice and this permission notice shall be included in
+// *   all copies or substantial portions of the Software.
+// *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// *   THE SOFTWARE.
+// * 
+// **********************************************************************************using System;
+
+using Microsoft.EntityFrameworkCore;
+using ChocAn.Data;
+using ChocAn.Repository;
+using ChocAn.Repository.Paging;
+using ChocAn.AccountsPayableReportRepository;
+using ChocAn.RabbitMQ.Extensions.DependencyInjection;
+using ChocAn.RabbitMQMessages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// **************************************************************************
+// * Add services to the container.
+// **************************************************************************
+
+builder.Services.Configure<PagingOptions>(builder.Configuration.GetSection("DefaultPagingOptions"));
+
+// DbContext for accessing Member repository
+builder.Services.AddDbContextPool<AccountsPayableReportDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IRepository<AccountsPayableReport>, DefaultAccountsPayableReportRepository>();
+
+// **************************************************************************
+// * Add RabbitMQ services to the container.
+// **************************************************************************
+
+builder.Services.AddRabbitMQMessage<IRabbitMQMessage<AccountsPayableReport>, DefaultRabbitMQMessage<AccountsPayableReport>>(
+    "AccountsPayableReport-Create",
+    options =>
+    {
+        options.ExchangeUri = "amqp://guest:guest@localhost:5672";
+        options.ExchangeName = "ChocAn.AccountsPayableReportExchange";
+    });
+
+builder.Services.AddRabbitMQMessage<IRabbitMQMessage<ProviderTransactionsReport>, DefaultRabbitMQMessage<ProviderTransactionsReport>>(
+    "ProviderTransactionsReport-Create",
+    options =>
+    {
+        options.ExchangeUri = "amqp://guest:guest@localhost:5672";
+        options.ExchangeName = "ChocAn.ProviderTransactionsReportExchange";
+    });
+
+// **************************************************************************
+// * Add remaining services to the container.
+// **************************************************************************
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// **************************************************************************
+// * Configure the HTTP request pipeline.
+// **************************************************************************
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ChocAn.RabbitMQMessages/RabbitMQOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocAn.RabbitMQ.Extensions.DependencyInjection
+{
+    public class RabbitMQOptions
+    {
+        public string? ExchangeUri { get; set; }
+        public string? ExchangeName { get; set; }
+    }
+}
